3a770a8491b99dd706093aec3e0019d604dc0b85
Artem <leoartemlev@yandex.ru>
2021-09-25 11:49:33 +0500

    Создал первый прототип с простой генерацией уровня и его движением

---
+++ Assets/Scipts(Prototype)/LvlMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvlMovement : MonoBehaviour
+{
+    [SerializeField] float _speed;
+
+    public bool canMove = true;
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            this.transform.Translate(0, -_speed * Time.deltaTime, 0);
+        }
+    }
+}
