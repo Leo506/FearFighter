613629162ac27308b4976b872b3e3679e40a01d5
Artem <leoartemlev@yandex.ru>
2021-09-28 01:08:30 +0300

    Создал прототип с управлением при помощи свайпов

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] PlayerInput _input;
+    Rigidbody2D rb2D;
+    BoxCollider2D bx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        bx = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = _input.GetSwipe();
+        if (AttemptMove(dir))
+            rb2D.MovePosition(rb2D.position + dir);
+    }
+
+    bool AttemptMove(Vector2 dir)
+    {
+        Vector2 newPos = rb2D.position + dir;
+        bx.enabled = false;
+        RaycastHit2D hit = Physics2D.Linecast(rb2D.position, newPos);
+        bx.enabled = true;
+
+        if (hit.transform != null)
+            return false;
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/RoomGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject _floorPrefab, _wallPrefab;
+    public float xOffset;
+    public float yOffset;
+
+    public void GenerateRoom()
+    {
+        GameObject room = new GameObject("Room");
+        for (int i = -1; i <= 3; i++)
+        {
+            for (int j = -1; j <= 9; j++)
+            {
+                if (i < 0 || j < 0 || i == 3 || j == 9)
+                {
+                    var wll = Instantiate(_wallPrefab, room.transform);
+                    wll.transform.localPosition = new Vector2((i - 1) * xOffset, (j - 4) * yOffset);
+                }
+                else
+                {
+                    GameObject obj;
+
+                    if (Random.Range(0, 3) == 1)
+                        obj = Instantiate(_wallPrefab, room.transform);
+                    else
+                        obj = Instantiate(_floorPrefab, room.transform);
+
+                    obj.transform.localPosition = new Vector2((i - 1) * xOffset, (j - 4) * yOffset);
+                }
+
+
+            }
+        }
+    }
+}
