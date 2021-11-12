15aabaa30416ee7825520fabea4d48e5fb5e26b3
Artem <leoartemlev@yandex.ru>
2021-11-13 00:26:35 +0500

    Создал арену для боса; создал один первый тип атак боса

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum BossState
+{
+    FIRST_STATE,
+    SECOND_STATE
+}
+
+public class BossController : MonoBehaviour
+{
+    BossState currentState = BossState.FIRST_STATE;
+    public float delay = 3;  // Время между атаками
+    public float HP = 100;
+    public int countOfFire = 10;
+    public float distanceMultiplier = 1;
+
+    [SerializeField] FireBallController firePrefab;  // Префаб снаряда, который будет пускать босс
+
+    CircleCollider2D cc2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc2D = GetComponent<CircleCollider2D>();
+        StartCoroutine(Delay());
+    }
+
+    void Fire()
+    {
+        float radius = cc2D.radius * this.transform.localScale.x * distanceMultiplier;
+
+        for (int j = 1; j <= 3; j++)
+        {
+            for (int i = 0; i < countOfFire; i++)
+            {
+                float angle = i * 2 * Mathf.PI / countOfFire;
+                float x = Mathf.Cos(angle) * radius * (float)j/3 + this.transform.position.x;
+                float y = Mathf.Sin(angle) * radius * (float)j / 3 + this.transform.position.y;
+
+                var fireBall = Instantiate(firePrefab);
+                fireBall.transform.position = new Vector2(x, y);
+                fireBall.SetDir(fireBall.transform.position - this.transform.position);
+            }
+        }
+
+        StartCoroutine(Delay());
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(delay);
+        Fire();
+    }
+
+    
+}
---
+++ Assets/Scripts/FireBallController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallController : MonoBehaviour
+{
+    public float speed = 100;
+    Vector2 direction = Vector2.zero;
+
+    Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.isKinematic = true;
+        rb2d.useFullKinematicContacts = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    public void SetDir(Vector2 dir)
+    {
+        direction = dir;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.GetComponent<BossController>() == null && collision.collider.gameObject.GetComponent<FireBallController>() == null)
+            Destroy(this.gameObject);
+    }
+}
